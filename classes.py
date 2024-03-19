b80fcaa07c19d0a6e186cadbcbfd4bc6304373d4
aleksklepka <aleksanderklepka@gmail.com>
2024-03-19 01:49:35 +0100

    Created new classes.

---
+++ classes.py
@@ -0,0 +1,27 @@
+class AccidentData:
+    def __init__(self, location, publish_date, update_date, vehicles, casualties, casualties_age, injured, accident_reason, action_sequence, link):
+        self.location = location
+        self.publish_date = publish_date
+        self.update_date = update_date
+        self.vehicles = vehicles
+        self.num_vehicles = len(self.vehicles)
+        self.casualties = casualties
+        self.casualties_age = casualties_age
+        self.injured = injured
+        self.accident_reason = accident_reason
+        self.action_sequence = action_sequence
+        self.link = link
+
+
+class MetaData:
+    def __init__(self, publication_date, update_date, meta_location, title, html_text, raw_text, link):
+        self.publication_date = publication_date #done
+        self.update_date = update_date #done
+        self.meta_location = meta_location
+        self.title = title #done
+        self.HTML_text = html_text #done
+        self.raw_text = raw_text #done
+        self.link = link #done
+
+    def __str__(self):
+        return f"Title: {self.title} \nPublication Date: {self.publication_date} \nUpdate Date: {self.update_date} \nLocation: {self.meta_location} \nLink: {self.link}\n"
