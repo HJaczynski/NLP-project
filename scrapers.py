5d1c6ef37920f55cb4ad96118ec9b1ddd1548fe7
aleksklepka <aleksanderklepka@gmail.com>
2024-03-19 01:59:49 +0100

    Created functions for obtaning links via API and scraping the links.

---
+++ scrapers.py
@@ -0,0 +1,101 @@
+from bs4 import BeautifulSoup
+from classes import AccidentData, MetaData
+import requests
+import re
+import time
+from datetime import datetime
+
+# Regular expression pattern to match the date format
+def extract_date_from_string(text):
+    # Regular expression pattern to match the date format
+    pattern = r'([A-Za-z]+ \d{1,2}, \d{4}, \d{2}:\d{2} [AP]M)'
+    
+    match = re.search(pattern, text)
+    
+    if match:
+        date = match.group(1)
+        return date
+    else:
+        return None
+
+
+# Function to obtain the links to scrape from the UNB API
+def unb_api_fetcher():
+    link_id = 0
+    has_more = True
+    links_to_scrape = set()
+
+    keywords = ["road-crash", "road", "highwawy", "vehicle", 
+                "car", "truck", "bus", "motorcycle", "collision", 
+                "accident", "human-hauler"]
+
+    while has_more is True:
+        http_link = f"https://www.unb.com.bd/api/tag-news?tag_id=54&item={link_id}"
+        response = requests.get(http_link)
+        data = response.json()
+        has_more = data['hasMore']
+        html_content = data['html']
+        soup = BeautifulSoup(html_content, 'lxml')
+
+        for anchor in soup.find_all('a', href=True):
+            for keyword in keywords:
+                href = anchor.get('href')
+                if keyword in anchor['href'] and href.startswith('https://www.unb.com.bd/category/Bangladesh/'):
+                    links_to_scrape.add(anchor['href'])
+
+        
+        print(f"[{link_id}]: {has_more}")
+        link_id += 1
+        time.sleep(1)
+        
+    return list(links_to_scrape)
+
+
+# Function to scrape the UNB articles
+def scrape_metadata(links):
+    MetaDataList = []
+
+    for link in links:
+        title = ""
+        raw_text = ""
+        update_date = None
+
+        print(f"Scrapping [{link}]")
+
+        html_text = requests.get(link)
+        soup = BeautifulSoup(html_text.text, 'lxml')
+
+        information = soup.find_all('li', class_='news-section-bar')
+
+        # Extract the publication and update date
+        for info in information:
+            if "Publish" in info.text:
+                date_str = extract_date_from_string(info.text)
+                publication_date = datetime.strptime(date_str, '%B %d, %Y, %I:%M %p')
+            elif "Update" in info.text:
+                date_str = extract_date_from_string(info.text)
+                update_date = datetime.strptime(date_str, '%B %d, %Y, %I:%M %p')
+                break
+
+        # Extract the title of the article
+        title_div = soup.find('div', class_='upper-box')
+        if title_div:
+            h2_tag = title_div.find('h2')
+            if h2_tag:
+                title = h2_tag.text
+
+        # Extract the raw text from the article
+        div = soup.find('div', class_='text')
+        if div:
+            paragraphs = div.find_all('p')
+            print(len(paragraphs))
+            for p in paragraphs:
+                if not p.find('a'):
+                    raw_text += p.text + " "
+        
+        # Create a new MetaData object and append it to the list
+        NewMetaData = MetaData(publication_date, update_date, "Bangladesh", title, html_text.text, raw_text, link)
+        MetaDataList.append(NewMetaData)
+
+    # Return the list of MetaData objects
+    return MetaDataList
